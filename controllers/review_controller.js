33dacdf1de0420182342c6b4ee98b4b2b69566f5
Brian Wynn <brian.wynn83@gmail.com>
2016-07-17 21:40:32 -0700

    All review routes have been defined and are working as expected.

--- controllers/review_controller.js
+++ controllers/review_controller.js
@@ -3,41 +3,100 @@ const Review = require("../models/review_model");
 // get reviews
 // /get_reviews - GET
 exports.getReviews = function(req, res) {
+  Review.find(function(err, reviews) {
+    if (err) {
+      res.send(err);
+    }
 
+    res.json(reviews);
+  });
 };
 
 // get review by author
 // /get_review_author - POST
 exports.getReviewByAuthor = function(req, res) {
+  Review.find({authorID: req.body.authorID}, function(err, reviews) {
+    if (err) {
+      res.send(err);
+    }
 
+    res.json(reviews);
+  });
 };
 
 // get review by trail
 // /get_review_trail - POST
 exports.getReviewByTrail = function(req, res) {
+  Review.find({trailID: req.body.trailID}, function(err, reviews) {
+    if (err) {
+      res.send(err);
+    }
 
+    res.json(reviews);
+  })
 };
 
 // get review by ID
 // get_review - POST
 exports.getReviewByID = function(req, res) {
+  Review.findOne({_id: req.body.id}, function(err, review) {
+    if (err) {
+      res.send(err);
+    }
 
+    res.json(review);
+  });
 };
 
 // add review
 // /add_review - POST
 exports.addReview = function(req, res) {
 
+  const review = new Review();
+
+  review.authorID = req.session.passport.user;
+  review.trailID = req.body.trailID;
+  review.rating = req.body.rating;
+  review.comment = req.body.comment;
+  review.pictures = req.body.pictures;
+
+  review.save(function(err, review) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(review);
+  });
 };
 
 // update review
 // /update_review - PUT
 exports.updateReview = function(req, res) {
+  Review.find({authorID: req.session.passport.user}, function(err, reviews) {
+    Review.update({_id: req.body.reviewID}, {
+      rating: req.body.rating,
+      comment: req.body.comment,
+      pictures: req.body.pictures
+    }, function(err, review) {
+      if (err) {
+        res.send(err);
+      }
 
+      res.json(review);
+    });
+  });
 };
 
 // delete review
 // /delete_review - PUT
 exports.deleteReview = function(req, res) {
+  Review.find({authorID: req.body.userID}, function(err, review) {
+    Review.remove({_id: req.body.reviewID}, function(err, review) {
+      if (err) {
+        res.send(err);
+      }
 
+      res.json({message: "Review Successfully removed"});
+    });
+  });
 };
--- routes/review_routes.js
+++ routes/review_routes.js
@@ -1,6 +1,6 @@
 const reviewCtrl = require('../controllers/review_controller');
 
-exports.module = function(app, passport) {
+module.exports = function(app, passport) {
 
   // get all reviews
   app.get('/get_reviews', reviewCtrl.getReviews);
@@ -15,11 +15,20 @@ exports.module = function(app, passport) {
   app.post('/get_review', reviewCtrl.getReviewByID);
 
   // add review
-  app.post('/add_review', reviewCtrl.addReview);
+  app.post('/add_review', loggedIn, reviewCtrl.addReview);
 
   // update review
-  app.put('/update_review', reviewCtrl.updateReview);
+  app.put('/update_review', loggedIn, reviewCtrl.updateReview);
 
   // delete review
-  app.put('/delete_review', reviewCtrl.deleteReview);
+  app.put('/delete_review', loggedIn, reviewCtrl.deleteReview);
 };
+
+function loggedIn(req, res, next) {
+  // if authenticated
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/login');
+}
