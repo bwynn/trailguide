5c839fe71b4d469a79863c54ff79c856c349c540
Brian Wynn <brian.wynn83@gmail.com>
2016-07-24 23:18:20 -0700

    Menu toggle controller and styles updated to show and hide menu items.

--- public/js/controllers/main_controller.js
+++ public/js/controllers/main_controller.js
@@ -1,7 +1,23 @@
 angular.module('MainCtrl', [])
   .controller('mainController', ['$scope', '$rootScope', function($scope, $rootScope) {
 
+    // showMenu
+    $scope.showMenu = false;
+
+    // various state emits
     $rootScope.$on('loggedInEmit', function(e, args) {
       $rootScope.loggedIn = args.loggedIn;
     });
+
+    // toggle menu
+    $scope.toggleMenu = function() {
+      if ($scope.showMenu) {
+        $scope.showMenu = false;
+      } else if (!$scope.showMenu) {
+        $scope.showMenu = true;
+      } else {
+        console.log("Toggle Menu failed.");
+      }
+    };
+
   }]);
